84894beef0074b22c0a5b36a823db2817541cef8
Yovanny Zeballos - Inetum <yovanny.zeballos@inetum.com>
2024-07-05 17:06:44 -0500

    Correcion environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  api: 'http://localhost:3000/.netlify/functions/api',
+  api: 'https://api-encuestas.netlify.app/.netlify/functions/api',
   api_key: 'OQlK5B06LU1WPrRFspoP7GAlY8MFGCFhFRsyH6iqkw5XI4b6wGR3bLRmFrzVgzhDB0LdYUKFqDkZYr7zOCilY0I1v87EuSQONUx0a20E86uhhzlodQ3ur0E4EVrTTOw3'
 };
 
